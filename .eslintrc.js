61bc4d56f839b0f5c5e3978b3dde4ce0a0e327fd
Gabriel Cesar Mello <gabriel.mello@kabum.com.br>
2020-09-21 17:43:12 -0300

    Removed the trash, added some linter rules and started playing with TDD

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    camelcase: [1],
+    'arrow-parens': [2, 'always'],
+    "generator-star-spacing": ["error", { "before": false, "after": true }]
   },
 };
